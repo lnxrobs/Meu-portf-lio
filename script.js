4e3f24165552a3d6371bee70fdb3fbf27af7bf31
Robson Dariva <146855243+lnxrobs@users.noreply.github.com>
2025-04-11 16:34:29 +0000

    Update

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+window.addEventListener('scroll', () => {
+    const secoes = document.querySelectorAll('section');
+    secoes.forEach(secao => {
+        const rect = secao.getBoundingClientRect();
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+            secao.classList.add('visible');
+        } else {
+            secao.classList.remove('visible');
+        }
+    });
+});
+
+// Torne a primeira seção visível ao carregar a página
+window.addEventListener('load', () => {
+    const secoes = document.querySelectorAll('section');
+    if (secoes.length > 0) {
+        secoes[0].classList.add('visible');
+    }
+});
+
+
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListenes('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+
+})
